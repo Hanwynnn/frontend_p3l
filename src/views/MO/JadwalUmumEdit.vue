e4e2949fcc6cee40a9d11f9eb8dc914e8af38630
Hanwynnn <tanghanwyn@gmail.com>
2023-06-13 22:04:14 +0700

    Update JadwalUmumEdit.vue

--- src/views/MO/JadwalUmumEdit.vue
+++ src/views/MO/JadwalUmumEdit.vue
@@ -8,91 +8,29 @@
             <hr />
             <form @submit.prevent="update">
               <div class="form-group mb-3">
-                <label class="form-label">Nama Instruktur</label>
-                <input
-                  type="text"
+                <label for="content" class="form-label">Instruktur</label>
+                <select
                   class="form-control"
-                  v-model="instruktur.nama_instruktur"
-                  placeholder="Masukkan nama instruktur"
-                />
-                <!-- validation -->
-                <div
-                  v-if="validation.nama_instruktur"
-                  class="mt-2 alert alert-danger"
+                  v-model="jadwalumum.id_instruktur"
                 >
-                  {{ validation.nama_instruktur[0] }}
-                </div>
-              </div>
-
-              <div class="form-group mb-3">
-                <label for="content" class="form-label">Alamat</label>
-                <input
-                  class="form-control"
-                  v-model="instruktur.alamat"
-                  placeholder="Masukkan Alamat"
-                />
+                  <option
+                    v-for="instrukturs in instruktur"
+                    :key="instrukturs.ID_INSTRUKTUR"
+                    :value="instrukturs.ID_INSTRUKTUR"
+                    :selected="
+                      instrukturs.ID_INSTRUKTUR == jadwalumum.id_instruktur
+                    "
+                  >
+                    {{ instrukturs.NAMA_INSTRUKTUR }}
+                  </option>
+                  <div
+                    v-if="validation.ID_INSTRUKTUR"
+                    class="mt-2 alert alert-danger"
+                  >
+                    {{ validation.ID_INSTRUKTUR[0] }}
+                  </div>
+                </select>
                 <!-- validation -->
-                <div v-if="validation.alamat" class="mt-2 alert alert-danger">
-                  {{ validation.alamat[0] }}
-                </div>
-              </div>
-
-              <div class="form-group mb-3">
-                <label for="content" class="form-label">Telepon</label>
-                <input
-                  class="form-control"
-                  type="number"
-                  v-model="instruktur.no_telp"
-                  placeholder="Masukkan Nomor Telepon"
-                />
-                <!-- validation -->
-                <div v-if="validation.no_telp" class="mt-2 alert alert-danger">
-                  {{ validation.no_telp[0] }}
-                </div>
-              </div>
-
-              <div class="form-group mb-3">
-                <label for="content" class="form-label">Email</label>
-                <input
-                  class="form-control"
-                  v-model="instruktur.email"
-                  placeholder="Masukkan Email"
-                />
-                <!-- validation -->
-                <div v-if="validation.email" class="mt-2 alert alert-danger">
-                  {{ validation.email[0] }}
-                </div>
-              </div>
-
-              <div class="form-group mb-3">
-                <label for="content" class="form-label">Password</label>
-                <input
-                  type="password"
-                  class="form-control"
-                  v-model="instruktur.password"
-                  placeholder="Masukkan Password"
-                />
-                <!-- validation -->
-                <div v-if="validation.password" class="mt-2 alert alert-danger">
-                  {{ validation.password[0] }}
-                </div>
-              </div>
-
-              <div class="form-group mb-3">
-                <label for="content" class="form-label">Tanggal Lahir</label>
-                <input
-                  type="date"
-                  class="form-control"
-                  v-model="instruktur.tanggal_lahir"
-                  placeholder="Masukkan Tanggal Lahir"
-                />
-                <!-- validation -->
-                <div
-                  v-if="validation.tanggal_lahir"
-                  class="mt-2 alert alert-danger"
-                >
-                  {{ validation.tanggal_lahir[0] }}
-                </div>
               </div>
 
               <button type="submit" class="btn btn-primary">SIMPAN</button>
@@ -103,21 +41,20 @@
     </div>
   </div>
 </template>
-    
-    <script>
+      
+      <script>
 import { reactive, ref, onMounted } from "vue";
 import { useRouter, useRoute } from "vue-router";
 import axios from "axios";
 
 export default {
   setup() {
-    const instruktur = reactive({
-      nama_instruktur: "",
-      tanggal_lahir: "",
-      alamat: "",
-      email: "",
-      password: "",
-      no_telp: "",
+    let instruktur = ref([]);
+    const jadwalumum = reactive({
+      id_instruktur: "",      
+      id_kelas: "",
+      sesi_kelas: "",
+      hari_kelas: "",
     });
 
     const validation = ref([]);
@@ -126,43 +63,59 @@ export default {
 
     onMounted(() => {
       axios
-        .get("https://gofitbackend.hanwyntugas.com/backend/public/api/instruktur/" + route.params.id, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        })
+        .get(
+          "https://gofitbackend.hanwyntugas.com/backend/public/api/jadwalumum/" +
+            route.params.id,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        )
         .then((response) => {
-          instruktur.nama_instruktur = response.data.data.NAMA_INSTRUKTUR;
-          instruktur.tanggal_lahir =
-            response.data.data.TANGGAL_LAHIR_INSTRUKTUR;
-          instruktur.alamat = response.data.data.ALAMAT_INSTRUKTUR;
-          instruktur.email = response.data.data.EMAIL_INSTRUKTUR;
-          instruktur.password = response.data.data.password;
-          instruktur.no_telp = response.data.data.TELEPON_INSTRUKTUR;
+          //assign state posts with response data
+          jadwalumum.id_instruktur = response.data.data.ID_INSTRUKTUR;
+          jadwalumum.sesi_kelas = response.data.data.SESI_KELAS;
+          jadwalumum.hari_kelas = response.data.data.HARI_KELAS;
+          jadwalumum.id_kelas = response.data.data.ID_KELAS;
+          console.log(jadwalumum.id_instruktur);
         })
         .catch((error) => {
-          console.log(error);
+          console.log(error.response.data);
+        });
+    });
+
+    onMounted(() => {
+      //get API from Laravel Backend
+      axios
+        .get(
+          "https://gofitbackend.hanwyntugas.com/backend/public/api/instruktur",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        )
+        .then((response) => {
+          //assign state posts with response data
+          instruktur.value = response.data.data;
+          console.log(instruktur.value);
+        })
+        .catch((error) => {
+          console.log(error.response.data);
         });
     });
 
     function update() {
-      let nama_instruktur = instruktur.nama_instruktur;
-      let tanggal_lahir = instruktur.tanggal_lahir;
-      let alamat = instruktur.alamat;
-      let email = instruktur.email;
-      let password = instruktur.password;
-      let no_telp = instruktur.no_telp;
-
       axios
         .put(
-          "https://gofitbackend.hanwyntugas.com/backend/public/api/instruktur/" + route.params.id,
+          "https://gofitbackend.hanwyntugas.com/backend/public/api/jadwalumum/" +
+            route.params.id,
           {
-            NAMA_INSTRUKTUR: nama_instruktur,
-            TANGGAL_LAHIR_INSTRUKTUR: tanggal_lahir,
-            ALAMAT_INSTRUKTUR: alamat,
-            EMAIL_INSTRUKTUR: email,
-            password: password,
-            TELEPON_INSTRUKTUR: parseInt(no_telp),
+            ID_INSTRUKTUR: jadwalumum.id_instruktur,
+            ID_KELAS: jadwalumum.id_kelas,
+            SESI_KELAS: jadwalumum.sesi_kelas,
+            HARI_KELAS: jadwalumum.hari_kelas,
           },
           {
             headers: {
@@ -171,14 +124,16 @@ export default {
           }
         )
         .then(() => {
-          router.push({ name: "instruktur.index" });
+          router.push({ name: "jadwalumum.index" });
         })
         .catch((error) => {
-          validation.value = error.response.data.errors;
+          validation.value = error.response.data;
+          console.log(validation.value);
         });
     }
     return {
       instruktur,
+      jadwalumum,
       validation,
       update,
       router,
@@ -186,5 +141,5 @@ export default {
   },
 };
 </script>
-    <style>
+      <style>
 </style> 
